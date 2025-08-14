e3c16a762c6d84e14f8c2a82f8be6ab54b556123
Henry Cerbone <henry.cerbone@biology.ox.ac.uk>
2025-08-14 12:11:20 +0100

    update directory structure to split out pdf processing and update imports accordingly.

--- metabeeai_llm/__init__.py
+++ metabeeai_llm/__init__.py
@@ -1,6 +1,6 @@
 # metabeeai_llm/__init__.py
-from .split_pdf import split_pdfs
-from .va_process_papers import process_papers
+from process_pdfs.split_pdf import split_pdfs
+from process_pdfs.va_process_papers import process_papers
 from .unique_chunk_id import check_chunk_ids_in_pages_dir
 from .process_llm_output import process_papers as extract_data
 from .llm_pipeline import get_literature_answers, merge_json_in_the_folder
